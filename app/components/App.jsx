e2d554d0054ad93ce3965b9eaf18da77e9fee818
Nic Young <nryoung@gmail.com>
2016-09-13 18:04:54 -0600

    add components, add, and delete functionality

---
+++ app/components/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import uuid from 'uuid';
+import Notes from './Notes';
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+  }
+  render() {
+    const {notes} = this.state;
+    return (
+      <div>
+        <button onClick={this.addNote}>+</button>
+        <Notes notes={notes} onDelete={this.deleteNote} />
+      </div>
+    );
+  }
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
+  }
+  deleteNote = (id, e) => {
+    // Avoid bubbling to edit
+    e.stopPropagation();
+
+    this.setState({
+      notes: this.state.notes.filter(note => note.id !== id)
+    })
+  }
+}
